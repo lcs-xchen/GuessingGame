d94d7e57a14505ef23c14db165051534cd25f250
Cindy Chen <xchen@lcs.on.ca>
2024-03-29 17:44:31 -0400

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Cindy Chen on 2024-03-29.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
